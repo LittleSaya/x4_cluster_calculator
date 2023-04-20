37f6a53a80da2626eb19033615491141a1d33672
april <aprilisarabbit@gmail.com>
2023-04-20 08:35:41 +0800

    remove openlayers, add three.js

--- site/main.js
+++ site/main.js
@@ -1,72 +1,25 @@
-import Map from 'ol/Map.js'
-import View from 'ol/View.js'
-import VectorLayer from 'ol/layer/Vector.js'
-import VectorSource from 'ol/source/Vector.js'
-import PointGeom from 'ol/geom/Point.js'
-import Feature from 'ol/Feature.js'
-import * as style from 'ol/style.js'
-import * as color from 'ol/color.js'
+import * as THREE from 'three';
 
-import x4map from '../full-map.json'
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 0, 100);
+camera.lookAt(0, 0, 0);
 
-const mapLayer = new VectorLayer({
-  zIndex: 0,
-  source: new VectorSource(),
-  style: function (feature, reso) {
-    return new style.Style({
-      image: new style.Circle({
-        radius: 13,
-        fill: new style.Fill({
-          color: color.fromString('green'),
-        }),
-        stroke: new style.Stroke({
-          color: color.fromString('green'),
-          width: 1,
-        }),
-        zIndex: 0
-      }),
-      text: new style.Text({
-        text: feature.getProperties().label,
-        fill: new style.Fill({
-          color: 'black'
-        }),
-        font: '14px sans-serif',
-        textBaseline: 'middle',
-        offsetY: 2
-      }),
-      zIndex: 1
-    })
-  }
-});
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-const factoryLayer = new VectorLayer({
-  zIndex: 1,
-  source: new VectorSource()
-});
+const scene = new THREE.Scene();
 
-const map = new Map({
-  target: 'map_container',
-  layers: [
-    mapLayer,
-    factoryLayer
-  ],
-  view: new View({
-    center: [0, 0],
-    zoom: 2,
-  }),
-});
+const material = new THREE.LineBasicMaterial( { color: 0x0000ff, linewidth: 100 } );
 
-// 绘制地图框架
-for (const clusterId in x4map) {
-  const cluster = x4map[clusterId];
-  let name;
-  for (const fullSectorId in cluster.sectors) {
-    const sector = cluster.sectors[fullSectorId];
-    name = sector.name;
-    break;
-  }
-  const coordinate = cluster.coordinate;
-  const feature = new Feature(new PointGeom(coordinate));
-  feature.setProperties({ label: name });
-  mapLayer.getSource().addFeature(feature);
-}
+const points = [];
+points.push( new THREE.Vector3( - 10, 0, 0 ) );
+points.push( new THREE.Vector3( 0, 10, 0 ) );
+points.push( new THREE.Vector3( 10, 0, 0 ) );
+
+const geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+const line = new THREE.Line(geometry, material);
+
+scene.add( line );
+renderer.render( scene, camera );
