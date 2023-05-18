5f2b53742af76ab0f1f50100621dbbac68633a08
april <aprilisarabbit@gmail.com>
2023-05-19 02:17:44 +0800

    调整鼠标滚轮速度和WASD移动速度

--- site/util/CustomOrbitControls.js
+++ site/util/CustomOrbitControls.js
@@ -61,7 +61,7 @@ class OrbitControls extends EventDispatcher {
 		// This option actually enables dollying in and out; left as "zoom" for backwards compatibility.
 		// Set to false to disable zooming
 		this.enableZoom = true;
-		this.zoomSpeed = 1.0;
+		this.zoomSpeed = 3.0;
 
 		// Set to false to disable rotating
 		this.enableRotate = true;
